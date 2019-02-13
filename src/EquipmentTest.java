435f335181d69091ae19cf6a49dcedf210f1ac5b
will7hughes <will7hughes@gmail.com>
2019-02-12 22:04:56 -0600

    Created tests for EquipmentTest getters

--- src/EquipmentTest.java
+++ src/EquipmentTest.java
@@ -16,4 +16,35 @@ class EquipmentTest {
 
 		Assert.assertEquals("Name: Bucky, Number: 2, Weight: 3.30 lbs, Price: $4.40 - BadBoy", equip.toString());
 	}
+
+
+	void testGetName() throws AssertException {
+		Equipment equip = new Equipment("Bucky/2,3.3,4.4,BadBoy");
+		
+		Assert.assertEquals("Bucky", equip.getName());
+	}
+
+	void testGetCount() throws AssertException {
+		Equipment equip = new Equipment("Bucky/2,3.3,4.4,BadBoy");
+		
+		Assert.assertEquals(2, equip.getCount());
+	}
+
+	void testGetTotalWeight() throws AssertException {
+		Equipment equip = new Equipment("Bucky/2,3.3,4.4,BadBoy");
+		
+		Assert.assertEquals(3.3, equip.getTotalWeight());
+	}
+
+	void testGetTotalPrice() throws AssertException {
+		Equipment equip = new Equipment("Bucky/2,3.3,4.4,BadBoy");
+		
+		Assert.assertEquals(4.4, equip.getTotalPrice());
+	}
+
+	void testGetDescription() throws AssertException {
+		Equipment equip = new Equipment("Bucky/2,3.3,4.4,BadBoy");
+		
+		Assert.assertEquals("BadBoy", equip.getDescription());
+	}
 }
