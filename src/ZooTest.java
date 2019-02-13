8fc45592d920543269c0a2c8086b1bb73ff672de
will7hughes <will7hughes@gmail.com>
2019-02-12 22:06:23 -0600

    Created test for ZooTest addAnimal

--- src/ZooTest.java
+++ src/ZooTest.java
@@ -8,4 +8,19 @@ class ZooTest {
 		Assert.assertEquals(5, zoo.getCapacity());
 	}
 
+	void testAddAnimal() throws AssertException {
+		Zoo zoo = new Zoo(1);
+		zoo.addAnimal(new Animal());
+		zoo.addAnimal(new Animal());
+		
+		// Test increase in capacity
+		Assert.assertEquals(2, zoo.getCapacity());
+
+		Zoo pupperZoo = new Zoo(1);
+		Animal pupper = new Animal("Blue", "Pupper", 5.5, 6.0);
+		pupperZoo.addAnimal(pupper);
+		
+		// Test animal is added to array
+		Assert.assertEquals(pupper, pupperZoo.getAnimals()[0]);
+	}
 }
