2b6fa4238729812328c90a4286e7f07e5a60e231
will7hughes <will7hughes@gmail.com>
2019-02-12 22:08:59 -0600

    Filled out completion times in Todo

--- src/Todo.java
+++ src/Todo.java
@@ -1,18 +1,30 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 12
+Last Modified_______: 2019-02-12
+OU ID_______________: 113392078
+OU EMAIL____________: willhughes@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Animal Test Methods
+2. Animal equals Method
+3. Zoo Test Methods
+4. Zoo equals method
+5. Equipment Test Methods
+6. Equipment equals method
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 20
+2. 10
+3. 20
+4. 15
+5. 20
+6. 20
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 14
+2. 18
+3. 28
+4. 16
+5. 8
+6. 18
 */}
